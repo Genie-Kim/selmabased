ab56336082ff3d1222c15bc73e6d63af6e802ad7
Genie-Kim <kimjin928@naver.com>
2024-05-31 02:27:30 +0000

    LongCLIP model avilable

---
+++ models/LongCLIP/__init__.py
@@ -0,0 +1,2 @@
+from .longclip import *
+from .model_longclip import LongCLIP
---
+++ models/rpg_models/__init__.py
@@ -0,0 +1,5 @@
+
+from .cross_attention import *
+
+from .RegionalDiffusion_base import *
+from .RegionalDiffusion_xl import *
