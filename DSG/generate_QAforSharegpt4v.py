5f3f35263a783def2ae0f1ce93e7459812b35a99
Genie-Kim <kimjin928@naver.com>
2024-06-05 08:36:08 +0000

    QA generation done, wandb inference result to table done

--- DSG/generate_QAforSharegpt4v.py
+++ DSG/generate_QAforSharegpt4v.py
@@ -88,103 +88,193 @@ import re
 # print(f"Sampled image paths have been written to {output_file_path}.")
 
 
-# =====================================
-# Task 2: Question generation
-# =====================================
+# # =====================================
+# # Task 2: Question generation
+# # =====================================
 
-# Reading API KEY from _OAI_KEY.txt
-base_dir = "/home/compu/JinProjects/jinprojects/SELMA/datasets/ShareGPT4V/data"
-merged_json_path = 'sharegpt4v/500sampledlist_share-captioner_coco_lcs_sam_1246k_1107.json'
-merged_json_path = os.path.join(base_dir, merged_json_path)
+# # Reading API KEY from _OAI_KEY.txt
+# base_dir = "/home/compu/JinProjects/jinprojects/SELMA/datasets/ShareGPT4V/data"
+# sampled_json_path = 'sharegpt4v/500sampledlist_share-captioner_coco_lcs_sam_1246k_1107.json'
+# output_json_path='datasets/ShareGPT4V/data/sharegpt4v/DSG_generated_sharegpt4v_instruct_gpt4-vision_cap100k.json'
+# error_json_path = 'datasets/ShareGPT4V/data/sharegpt4v/DSG_generated_sharegpt4v_instruct_gpt4-vision_cap100k_Errorlist.json'
+# sampled_json_path = os.path.join(base_dir, sampled_json_path)
 
-groq_setup()
-print(groq_completion(
-    'hello, how are you doing?',
-))
+# groq_setup()
+# print(groq_completion(
+#     'hello, how are you doing?',
+# ))
 
-openai_setup()
-print(openai_completion(
-    'hello, how are you doing?',
-))
+# openai_setup()
+# print(openai_completion(
+#     'hello, how are you doing?',
+# ))
 
-with open(merged_json_path, 'r') as f:
-    json_data = json.load(f)
+# with open(sampled_json_path, 'r') as f:
+#     json_data = json.load(f)
 
-keywordpattern  = r'\((.*?)\)'
-output_json_path='my_csv.json'
-error_json_path = 'errorlist.json'
-for folder, datalist in json_data.items():
-    for data in datalist:
-        error_list = {}
-        output_dump_list = {}
+# keywordpattern  = r'\((.*?)\)'
+
+# for folder, datalist in json_data.items():
+#     for data in datalist:
+#         error_list = {}
+#         output_dump_list = {}
         
         
-        id = data['id']
-        imagepath = data['image']
-        INPUT_TEXT_PROMPT = data['conversations'][1]['value'].replace('\n\n','\n')
-        id2prompts = {
-            'custom_0': {
-                'input': INPUT_TEXT_PROMPT,
-            }
-        }
+#         id = data['id']
+#         imagepath = data['image']
+#         INPUT_TEXT_PROMPT = data['conversations'][1]['value'].replace('\n\n','\n')
+#         id2prompts = {
+#             'custom_0': {
+#                 'input': INPUT_TEXT_PROMPT,
+#             }
+#         }
 
-        id2tuple_outputs, id2question_outputs, id2dependency_outputs = generate_dsg(
-            id2prompts,
-            # you can change this method with any method that takes prompt as input and outputs LLM generation result.
-            generate_fn=groq_completion
-        )
-        try:
-            qid2tuple = parse_tuple_output(id2tuple_outputs['custom_0']['output'])
-            qid2dependency = parse_dependency_output(id2dependency_outputs['custom_0']['output'])
-            qid2question = parse_question_output(id2question_outputs['custom_0']['output'])
+#         id2tuple_outputs, id2question_outputs, id2dependency_outputs = generate_dsg(
+#             id2prompts,
+#             # you can change this method with any method that takes prompt as input and outputs LLM generation result.
+#             generate_fn=groq_completion
+#         )
+#         try:
+#             qid2tuple = parse_tuple_output(id2tuple_outputs['custom_0']['output'])
+#             qid2dependency = parse_dependency_output(id2dependency_outputs['custom_0']['output'])
+#             qid2question = parse_question_output(id2question_outputs['custom_0']['output'])
             
-            question_number = len(qid2question)
-            qid2tuple = {k: v for k, v in qid2tuple.items() if k <= question_number}
-            qid2dependency = {k: v for k, v in qid2dependency.items() if k <= question_number}
+#             question_number = len(qid2question)
+#             qid2tuple = {k: v for k, v in qid2tuple.items() if k <= question_number}
+#             qid2dependency = {k: v for k, v in qid2dependency.items() if k <= question_number}
                             
-            tupdict = {}
-            keyworddict = {}
-            output_dump_list[id]={
-            'gpt4vcaption': INPUT_TEXT_PROMPT,
-            'imagepath': imagepath,
-            'question_list':[]
-            }
-            for tup in id2tuple_outputs['custom_0']['output'].strip().split('\n'):
-                id_temp = int(tup.split('|')[0].strip())
-                tupdict[id_temp]=tup.split('|')[1].strip()
-                keyworddict[id_temp] = re.search(keywordpattern, tupdict[id_temp]).group(1)
+#             tupdict = {}
+#             keyworddict = {}
+#             output_dump_list[id]={
+#             'gpt4vcaption': INPUT_TEXT_PROMPT,
+#             'imagepath': imagepath,
+#             'question_list':[]
+#             }
+#             for tup in id2tuple_outputs['custom_0']['output'].strip().split('\n'):
+#                 id_temp = int(tup.split('|')[0].strip())
+#                 tupdict[id_temp]=tup.split('|')[1].strip()
+#                 keyworddict[id_temp] = re.search(keywordpattern, tupdict[id_temp]).group(1)
             
-            for i in range(1, question_number+1):
-                keywords = keyworddict[i]
-                proposition_id = str(i)
-                dependency = ','.join([str(idx) for idx  in qid2dependency[i]])
-                category_broad = qid2tuple[i].split('-')[0].strip()
-                category_detailed = qid2tuple[i].split('-')[1].strip()
-                tuples = tupdict[i]
-                question_natural_language = qid2question[i]
-                output_dump_list[id]['question_list'].append({
-                    'keywords': keywords,
-                    'proposition_id': proposition_id,
-                    'dependency': dependency,
-                    'category_broad': category_broad,
-                    'category_detailed': category_detailed,  
-                    'tuples': tuples,
-                    'question_natural_language': question_natural_language
-                })
-            with open(output_json_path , 'a') as f:
-                json.dump(output_dump_list, f, indent=4)
-                f.write(', \n')
-            print(f"Successfully generated questions for image {id}.\n\n\n")
+#             for i in range(1, question_number+1):
+#                 keywords = keyworddict[i]
+#                 proposition_id = str(i)
+#                 dependency = ','.join([str(idx) for idx  in qid2dependency[i]])
+#                 category_broad = qid2tuple[i].split('-')[0].strip()
+#                 category_detailed = qid2tuple[i].split('-')[1].strip()
+#                 tuples = tupdict[i]
+#                 question_natural_language = qid2question[i]
+#                 output_dump_list[id]['question_list'].append({
+#                     'keywords': keywords,
+#                     'proposition_id': proposition_id,
+#                     'dependency': dependency,
+#                     'category_broad': category_broad,
+#                     'category_detailed': category_detailed,  
+#                     'tuples': tuples,
+#                     'question_natural_language': question_natural_language
+#                 })
+#             with open(output_json_path , 'a') as f:
+#                 json.dump(output_dump_list, f, indent=4)
+#                 f.write(', \n')
+#             print(f"Successfully generated questions for image {id}.\n\n\n")
 
-        except Exception as e:
-            error_list[id] = {}
-            error_list[id]['id2tuple'] = id2tuple_outputs['custom_0']['output']
-            error_list[id]['id2question'] = id2question_outputs['custom_0']['output']
-            error_list[id]['id2dependency'] = id2dependency_outputs['custom_0']['output']
-            error_list[id]['error_msg'] = str(e)
-            with open(error_json_path, 'a') as f:
-                json.dump(error_list, f, indent=4)
-                f.write(', \n')
+#         except Exception as e:
+#             error_list[id] = {}
+#             error_list[id]['id2tuple'] = id2tuple_outputs['custom_0']['output']
+#             error_list[id]['id2question'] = id2question_outputs['custom_0']['output']
+#             error_list[id]['id2dependency'] = id2dependency_outputs['custom_0']['output']
+#             error_list[id]['error_msg'] = str(e)
+#             with open(error_json_path, 'a') as f:
+#                 json.dump(error_list, f, indent=4)
+#                 f.write(', \n')
                 
             
-        time.sleep(6)
+#         time.sleep(6)
+# After the json file generated, you should add [,] to the beginning and end of the file.
+
+
+# =====================================
+# Task 3: Check the generated questions file.
+# =====================================
+
+def isEngNum(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+    
+output_json_path='datasets/ShareGPT4V/data/sharegpt4v/DSG_generated_sharegpt4v_instruct_gpt4-vision_cap100k.json'
+error_json_path = 'datasets/ShareGPT4V/data/sharegpt4v/DSG_generated_sharegpt4v_instruct_gpt4-vision_cap100k_Errorlist.json'
+sampled_json_path = 'datasets/ShareGPT4V/data/sharegpt4v/500sampledlist_share-captioner_coco_lcs_sam_1246k_1107.json'
+
+with open(output_json_path,'r') as f:
+    DSG_sharegpt4v_data = json.load(f)
+    
+with open(error_json_path,'r') as f:
+    DSG_sharegpt4v_Error_data = json.load(f)
+
+with open(sampled_json_path,'r') as f:
+    origin_data = json.load(f)
+    
+print(f"Successfully generated questions for {len(DSG_sharegpt4v_data)} images.")
+print(f"Failed to generate questions for {len(DSG_sharegpt4v_Error_data)} images.")
+
+origin_data_dict = {}
+for folder,dictlist in origin_data.items():
+    for dict1 in dictlist:
+        tempdict = dict1.copy()
+        id = tempdict.pop('id')
+        value = tempdict
+        if folder not in origin_data_dict:
+            origin_data_dict[folder] = {}
+        origin_data_dict[folder][id] = value
+
+
+print([(k,len(v)) for k,v in origin_data.items()])
+print([(k,len(v)) for k,v in origin_data_dict.items()])
+
+
+idlist = []
+for k,v in origin_data.items():
+    for item in v:
+        idlist.append(item['id'])
+# count the histogram of id in idlist
+idhist = {x:idlist.count(x) for x in idlist}
+# print all the id with count higher than 1
+print({k:v for k,v in idhist.items() if v>1})
+dup_ids = {k:v for k,v in idhist.items() if v>1}.keys()
+
+
+# change the DSG_sharegpt4v_data list to dictionary with folder name as key and id as subkey
+DSG_sharegpt4v_dict = {}
+for item in DSG_sharegpt4v_data:
+    for id,row in item.items():
+        if id not in dup_ids: # duplicate id from the original data should be removed
+            if isEngNum(row['gpt4vcaption']):
+                folder = row['imagepath'].split('/')[0]
+                if folder not in DSG_sharegpt4v_dict:
+                    DSG_sharegpt4v_dict[folder] = {}
+                DSG_sharegpt4v_dict[folder][id] = row
+
+
+print([len(v) for k,v in DSG_sharegpt4v_dict.items()])
+
+with open('datasets/ShareGPT4V/data/sharegpt4v/Dict_DSG_nondupid_generated_sharegpt4v_instruct_gpt4-vision_cap100k.json','w') as f:
+    json.dump(DSG_sharegpt4v_dict, f, indent=4)
+
+
+
+# change the DSG_sharegpt4v_Error_data list to dictionary with folder name as key and id as subkey
+error_dict = {}
+for item in DSG_sharegpt4v_Error_data:
+    for id,row in item.items():
+        for k,v in origin_data_dict.items():
+            if id in v:
+                folder = k
+                if folder not in error_dict:
+                    error_dict[folder] = {}
+                error_dict[folder][id] = row
+        
+print([len(v) for k,v in error_dict.items()])
+        
--- img_gen_merge.py
+++ img_gen_merge.py
@@ -1,5 +1,9 @@
 from diffusers import StableDiffusionPipeline, StableDiffusionXLPipeline
-from diffusers.schedulers import KarrasDiffusionSchedulers,DPMSolverMultistepScheduler
+from diffusers.schedulers import (
+    KarrasDiffusionSchedulers,
+    DPMSolverMultistepScheduler,
+    DDIMScheduler,
+)
 from models.LongCLIP import longclip
 import shutil
 import torch
@@ -8,70 +12,89 @@ import csv
 import os
 from tqdm import tqdm
 import argparse
-from models.RepeatTextencStableDiffusion_base import RepeatTextencStableDiffusionPipeline
+from models.RepeatTextencStableDiffusion_base import (
+    RepeatTextencStableDiffusionPipeline,
+)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_id', type=str, default="runwayml/stable-diffusion-v1-5")
-parser.add_argument('--prompt_path', type=str, default="datasets/ShareGPT4V/data/sharegpt4v/sharegpt4v_instruct_gpt4-vision_cap100k.json")
-parser.add_argument('--output_path', type=str, default="longtext_imggen")
-parser.add_argument('--cache_dir', type=str, default="/home/compu/JinProjects/jinprojects/SELMA/pretrained_models")
-parser.add_argument('--eval_benchmark', type=str, default="DSG")
-parser.add_argument('--batch_size', type=int, default=1)
-parser.add_argument('--text_enc', type=str, default="clip")
-parser.add_argument('--repeat_textenc', type=bool, default=False)
+parser.add_argument("--model_id", type=str, default="runwayml/stable-diffusion-v1-5")
+parser.add_argument(
+    "--prompt_path",
+    type=str,
+    default="datasets/ShareGPT4V/data/sharegpt4v/Dict_DSG_nondupid_generated_sharegpt4v_instruct_gpt4-vision_cap100k.json",
+)
+parser.add_argument("--output_path", type=str, default="exp_results")
+parser.add_argument(
+    "--cache_dir",
+    type=str,
+    default="/home/compu/JinProjects/jinprojects/SELMA/pretrained_models",
+)
+parser.add_argument("--eval_benchmark", type=str, default="DSG")
+parser.add_argument("--batch_size", type=int, default=16)
+parser.add_argument("--text_enc", type=str, default="clip")
+parser.add_argument("--repeat_textenc", type=bool, default=False)
 
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
-
 # model_id = "stabilityai/stable-diffusion-2"
 # model_id = "stabilityai/stable-diffusion-xl-base-1.0"
 # model_id = "CompVis/stable-diffusion-v1-4"
 # model_id = "runwayml/stable-diffusion-v1-5"
 model_id = args.model_id
-output_path = args.output_path
+output_foldername = args.text_enc + f"_repeat_textenc_{args.repeat_textenc}"
+output_path = os.path.join(args.output_path, model_id.split("/")[-1], output_foldername)
 batch_size = args.batch_size
-dataset_path = "/home/compu/JinProjects/jinprojects/SELMA/datasets/ShareGPT4V/data"
+seed=2468
 
-assert model_id!="stabilityai/stable-diffusion-2" or args.text_enc!='longclip', "LongCLIP model is not supported SD v2.1"
 
-if args.text_enc == 'longclip':
-    longclip_modelpath= os.path.join(args.cache_dir,"LongCLIP/longclip-L.pt")
-    
+# TODO: XL 구현, repeatedclip batch 구현.
+
+assert (
+    model_id != "stabilityai/stable-diffusion-2" or args.text_enc != "longclip"
+), "LongCLIP model is not supported SD v2.1"
+
+if args.text_enc == "longclip":
+    longclip_modelpath = os.path.join(args.cache_dir, "LongCLIP/longclip-L.pt")
+
 else:
-    longclip_modelpath= None
+    longclip_modelpath = None
 
 
 if "xl" in model_id:
     print("not implemented")
 
-    #     pipe = RepeatTextencStableDiffusionPipeline.from_pretrained(
-    #         model_id,
-    #         torch_dtype=torch.float16,
-    #         cache_dir=args.cache_dir,
-    #         safety_checker=None,
-    #         seed=2468,
-    #         repeat_textenc=args.repeat_textenc,
-    #     )
-    
+    pipe = RepeatTextencStableDiffusionPipeline.from_pretrained(
+        model_id,
+        torch_dtype=torch.float16,
+        cache_dir=args.cache_dir,
+        safety_checker=None,
+        repeat_textenc=args.repeat_textenc,
+    )
+    height=1024
+    width=1024
+
 else:
     pipe = RepeatTextencStableDiffusionPipeline.from_pretrained(
         model_id,
         torch_dtype=torch.float16,
         cache_dir=args.cache_dir,
         safety_checker=None,
-        seed=2468,
         repeat_textenc=args.repeat_textenc,
         longclip_modelfile=longclip_modelpath,
     )
+    height=512
+    width=512
+    # TODO: add seed fix option....
     # # for match with rpg model's configuration.
     # num_inference_steps=20,  # sampling step
     # height=1024,
     # width=1024,
     # guidance_scale=7.0,
 
-pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+# pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
 pipe.to(device)
 
 data_all = dict()
@@ -80,68 +103,75 @@ with open(args.prompt_path) as f:
     data = json.load(f)
 f.close()
 
-if not os.path.exists(args.output_path):
-    os.makedirs(args.output_path)
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
 
 ##############
 
 prompts = []
 names = []
-for i, item in enumerate(tqdm(data)):
-    prompt = item['conversations'][1]['value']
-    imageid = item['id']
-    imagepath = os.path.join(dataset_path,item['image'])
-    name = f"sharegpt4v_{imageid}"
-    prompts.append(prompt)
-    names.append(name)
+generator = torch.cuda.manual_seed_all(seed)
+for folder, data_dict in tqdm(data.items()):
+    count = 0
+    for imageid, value in tqdm(data_dict.items()):
+        prompt = value["gpt4vcaption"]
+        filename = os.path.basename(value["imagepath"])
+        name = imageid + os.path.splitext(filename)[-1]
+        prompts.append(prompt)
+        names.append(name)
+        count += 1
 
-    if len(prompts) == batch_size:
-        images = pipe(prompts).images
-        print(len(images))
-        for j, img in enumerate(images):
-            img.save(os.path.join(output_path, f"{names[j]}.jpg" ))
-            # TODO : copy the image from the imagepath to the output path
-            shutil.copy(imagepath, os.path.join(output_path, f"{names[j]}_original.jpg"))
+        if len(prompts) == batch_size:
+            images = pipe(
+                prompts,
+                num_inference_steps=50,
+                generator = generator
+                # height=height,
+                # width=width,
+            ).images
+            print(len(images))
+            for j, img in enumerate(images):
+                img.save(os.path.join(output_path, f"{names[j]}"))
+                # copy the image from the imagepath to the output path
+                # shutil.copy(imagepath, os.path.join(output_path, f"{names[j]}_original.jpg"))
 
-        prompts = []
-        names = []
-    
-    if i>10:
-        break
+            prompts = []
+            names = []
+
+        # if count > 100:
+        #     break
 
 
 #############
 
-if args.eval_benchmark == "DSG":
-    data_all = dict()
-    with open("DSG/dsg-1k-anns.csv") as f:
-        data = csv.reader(f, delimiter=",")
-        next(data)
-        for row in data:
-            if row[0] in data_all:
-                continue
-            data_all[row[0]] = row[1]
-    f.close()
-    print(len(data_all))
+# if args.eval_benchmark == "DSG":
+#     data_all = dict()
+#     with open("DSG/dsg-1k-anns.csv") as f:
+#         data = csv.reader(f, delimiter=",")
+#         next(data)
+#         for row in data:
+#             if row[0] in data_all:
+#                 continue
+#             data_all[row[0]] = row[1]
+#     f.close()
+#     print(len(data_all))
 
-    for k, v in tqdm(data_all.items()):
-        name = k
-        prompt = v
-        images = pipe(prompt, num_images_per_prompt=1, cross_attention_kwargs={"scale": scale / 100}).images[0]
-        images.save(os.path.join(output_path, f"{name}.jpg"))
+#     for k, v in tqdm(data_all.items()):
+#         name = k
+#         prompt = v
+#         images = pipe(prompt, num_images_per_prompt=1, cross_attention_kwargs={"scale": scale / 100}).images[0]
+#         images.save(os.path.join(output_path, f"{name}.jpg"))
 
-elif args.eval_benchmark == "TIFA":
-    with open("../TIFA/tifa_v1.0/tifa_v1.0_text_inputs.json", "r") as f:
-        data_all = json.load(f)
-    f.close()
+# elif args.eval_benchmark == "TIFA":
+#     with open("../TIFA/tifa_v1.0/tifa_v1.0_text_inputs.json", "r") as f:
+#         data_all = json.load(f)
+#     f.close()
 
-    for item in tqdm(data_all):
-        prompt = item["caption"]
-        name = item["id"]
-        images = pipe(prompt, num_images_per_prompt=1, cross_attention_kwargs={"scale": scale / 100}).images[0]
-        images.save(os.path.join(output_path, f"{name}.jpg"))
-
-
+#     for item in tqdm(data_all):
+#         prompt = item["caption"]
+#         name = item["id"]
+#         images = pipe(prompt, num_images_per_prompt=1, cross_attention_kwargs={"scale": scale / 100}).images[0]
+#         images.save(os.path.join(output_path, f"{name}.jpg"))
 
 
 # if args.text_enc == 'clip':
@@ -151,7 +181,7 @@ elif args.eval_benchmark == "TIFA":
 #     )
 
 # def get_textemb(prompt):
-    
+
 #     text_input = tokenizer(
 #         prompt, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt"
 #     )
@@ -161,7 +191,7 @@ elif args.eval_benchmark == "TIFA":
 
 #     with torch.no_grad():
 #         text_embeddings = text_encoder(text_input.input_ids.to(torch_device))[0]
-        
+
 #     max_length = text_input.input_ids.shape[-1]
 #     uncond_input = tokenizer([""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt")
 #     uncond_embeddings = text_encoder(uncond_input.input_ids.to(torch_device))[0]
@@ -169,13 +199,11 @@ elif args.eval_benchmark == "TIFA":
 #     return torch.cat([uncond_embeddings, text_embeddings])
 
 
-
 # from transformers.models.clip.modeling_clip import CLIPTextModel, add_start_docstrings, BaseModelOutputWithPooling, CLIPTextConfig
 # from typing import Any, Optional, Tuple, Union
 # @add_start_docstrings(
 #     """The repeated text model from CLIP without any head or projection on top."""
 # )
-
 
 
 # tokenizer = BertTokenizer.from_pretrained("cambridgeltl/SapBERT-from-PubMedBERT-fulltext")
